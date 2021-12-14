62d17792a39a9dc38feb5ba13fd429b6532cec35
Adarsh Kumar <adarshkumar245@gmail.com>
2021-12-14 15:07:52 +0530

    birthday-site almo fin

--- src/Intro.js
+++ src/Intro.js
@@ -1,20 +1,15 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { useState} from "react";
 import arziyaan from "./arziyaan.mp3";
 const Intro = () => {
-   const [audio, setAudio] = useState(null);
+   const [audio] = useState(new Audio(arziyaan));
    const play = () => {
       audio.play();
    }
 
-   useEffect(()=>{
-     setAudio(new Audio(arziyaan));
-   },[]);
-
    return <section className="intro">
        <h1>Happy Birthday Shabree/Shabreeshan/Arjun!</h1>
     <p class="subtitle">Yeh rha meri taraf se gift<br/>(Made from kabadi jaisa laptop)!</p>
-    <button onClick={()=>{play()}}>Play</button>
+    <button onClick={()=>{play()}}>Play(Arziyaan)</button>
    </section>
 }
 
